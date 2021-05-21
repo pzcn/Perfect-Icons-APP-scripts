faa68244097c07fade4e7eabeca812b3bf3295b6
PedroZ <sdustpedro@gmail.com>
2021-05-21 12:56:41 +0800

    1

--- scripts/misc/cache_size.sh
+++ scripts/misc/cache_size.sh
@@ -1,2 +1,2 @@
 cache_size=$((`du theme_files|awk '{print $1}'`))
-echo "当前缓存大小：$(printf '%.1f' `echo "scale=1;$cache_size/1024"|bc`) MB。出现问题时可以尝试清理缓存，清理后下次安装时会重新下载资源。"
+echo "当前缓存大小：$(printf '%.1f' `echo "scale=1;$cache_size/1024"|bc`) MB。"
--- scripts/misc/get_themes.sh
+++ scripts/misc/get_themes.sh
@@ -1,9 +1,9 @@
 if [ -d "/data/adb/modules_update/MIUIiconsplus" ]; then
-moduledir=/data/adb/modules_update/MIUIiconsplus
-echo "以 Magisk 模块形式进行安装，您已安装$theme，重启后应用。"
-elif [ -d "/data/adb/modules/MIUIiconsplus" ]
-moduledir=/data/adb/modules/MIUIiconsplus
-echo "以 Magisk 模块形式进行安装，当前已安装$theme。"
+source /data/adb/modules_update/MIUIiconsplus/module.prop
+echo "当前已安装：$theme，重启后应用。"
+elif [ -d "/data/adb/modules/MIUIiconsplus" ]; then
+source /data/adb/modules/MIUIiconsplus/module.prop
+echo "当前已安装：$theme。"
 else
-echo "以 Magisk 模块形式进行安装，当前未安装。"
+echo "当前未安装。"
 fi
