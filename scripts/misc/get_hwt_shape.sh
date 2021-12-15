4eeee33c5a7a75a42ff53aff78dc14fb46fae7c6
PedroZ <sdustpedro@gmail.com>
2021-12-15 11:22:58 +0800

    1

---
+++ scripts/misc/get_hwt_shape.sh
@@ -0,0 +1,4 @@
+source theme_files/hwt_config
+[ $hwt_shape = round ] && echo "当前选择：圆形"
+[ $hwt_shape = squarecircle ] && echo "当前选择：方圆形"
+[ $hwt_shape = rectangle ] && echo "当前选择：圆角矩形 "
---
+++ scripts/misc/get_hwt_size.sh
@@ -0,0 +1,5 @@
+source theme_files/hwt_config
+[ $hwt_size = small] && echo "当前选择：小"
+[ $hwt_size = medium ] && echo "当前选择：中"
+[ $hwt_size = large ] && echo "当前选择：较大 "
+[ $hwt_size = exlarge ] && echo "当前选择：最大（鸿蒙不推荐） "
