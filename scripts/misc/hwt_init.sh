6672491b0f7370d6643852aa25bf5405f828e193
pzcn <sdustpedro@gmail.com>
2021-12-16 00:05:33 +0800

    Update hwt_init.sh

--- scripts/misc/hwt_init.sh
+++ scripts/misc/hwt_init.sh
@@ -2,13 +2,13 @@
 [ -f "theme_files/hwt_size_config" ] || ( touch theme_files/hwt_size_config && echo "hwt_size=M" > theme_files/hwt_size_config )
 [ -f "theme_files/hwt_shape_config" ] || ( touch theme_files/hwt_shape_config && echo "hwt_shape=Rectangle" > theme_files/hwt_shape_config )
 
-if [[ ! -f "theme_files/hwt_shape_config" ]]; then
-touch theme_files/mtzdir_config
+if [[ ! -f "theme_files/hwt_dir_config" ]]; then
+touch theme_files/hwt_dir_config
 if [[ -d "/sdcard/Huawei/Theme" ]]; then
-echo "hwtdir=/sdcard/Huawei/Theme" > theme_files/mtzdir_config
+echo "hwtdir=/sdcard/Huawei/Theme" > theme_files/hwt_dir_config
 elif [[ -d "/sdcard/Download" ]]; then
-echo "hwtdir=/sdcard/Download" > theme_files/mtzdir_config
+echo "hwtdir=/sdcard/Download" > theme_files/hwt_dir_config
 else
-echo "hwtdir=/sdcard" > theme_files/mtzdir_config
+echo "hwtdir=/sdcard" > theme_files/hwt_dir_config
 fi
 fi
