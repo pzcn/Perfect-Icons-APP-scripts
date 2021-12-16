087fe3136d1ff79063a63ca1933657b181968cf5
PedroZ <sdustpedro@gmail.com>
2021-12-16 10:54:42 +0800

    Update hwt_init.sh

--- scripts/misc/hwt_init.sh
+++ scripts/misc/hwt_init.sh
@@ -3,12 +3,12 @@
 [ -f "theme_files/hwt_shape_config" ] || ( touch theme_files/hwt_shape_config && echo "hwt_shape=Rectangle" > theme_files/hwt_shape_config )
 
 if [[ ! -f "theme_files/hwt_dir_config" ]]; then
-touch theme_files/hwt_dir_config
-if [[ -d "/sdcard/Huawei/Theme" ]]; then
-echo "hwtdir=/sdcard/Huawei/Theme" > theme_files/hwt_dir_config
-elif [[ -d "/sdcard/Download" ]]; then
-echo "hwtdir=/sdcard/Download" > theme_files/hwt_dir_config
-else
-echo "hwtdir=/sdcard" > theme_files/hwt_dir_config
-fi
+	touch theme_files/hwt_dir_config
+	if [[ -d "/sdcard/Huawei/Themes" ]]; then
+		echo "hwtdir=/sdcard/Huawei/Themes" > theme_files/hwt_dir_config
+	elif [[ -d "/sdcard/Download" ]]; then
+		echo "hwtdir=/sdcard/Download" > theme_files/hwt_dir_config
+	else
+		echo "hwtdir=/sdcard" > theme_files/hwt_dir_config
+	fi
 fi
