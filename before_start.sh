e592eb94b1f62b1de0962d82541114f302f06b16
PedroZ <sdustpedro@gmail.com>
2021-12-21 22:50:42 +0800

    1

--- before_start.sh
+++ before_start.sh
@@ -1,17 +1,6 @@
-text="若本APP未显示新图标，则需要对EMUI/鸿蒙或MIUI完美图标进行更新。上线EMUI/鸿蒙OS主题导出功能，如有问题请及时反馈。"
-extract_dir="$START_DIR/online-scripts"
-remote_url="https://miuiicons-generic.pkg.coding.net/icons/files/script2.tar?version=latest"
-remote_config="https://miuiicons-generic.pkg.coding.net/icons/files/script2.ini?version=latest"
-addon_path=/sdcard/Documents/MIUI完美图标自定义
-[ -d "theme_files" ] || mkdir theme_files
-[ -f "theme_files/theme_config" ] || ( touch theme_files/theme_config && echo "sel_theme=default" > theme_files/theme_config )
-[ -f "theme_files/addon_config" ] || ( touch theme_files/addon_config && echo "addon=0" > theme_files/addon_config )
-[ -f "theme_files/mtzdir_config" ] || ( touch theme_files/mtzdir_config && echo "mtzdir=/sdcard/Download" > theme_files/mtzdir_config )
-[ -f "theme_files/hwt_theme_config" ] || ( touch theme_files/hwt_theme_config && echo "sel_theme=Aquamarine" > theme_files/hwt_theme_config )
-[ -f "theme_files/hwt_size_config" ] || ( touch theme_files/hwt_size_config && echo "hwt_size=M" > theme_files/hwt_size_config )
-[ -f "theme_files/hwt_shape_config" ] || ( touch theme_files/hwt_shape_config && echo "hwt_shape=Rectangle" > theme_files/hwt_shape_config )
-[ -f "theme_files/update_status.ini" ] && ( rm -rf theme_files/update_status.ini )
-[ -f "theme_files/download_config" ] || ( touch theme_files/download_config && echo "curlmode=0" > theme_files/download_config )
+
+remote_url="https://miuiicons-generic.pkg.coding.net/icons/files/script23.tar?version=latest"
+remote_config="https://miuiicons-generic.pkg.coding.net/icons/files/script23.ini?version=latest"
 
 download_latest_version() {
     echo "开始在线更新..."
@@ -44,5 +33,3 @@ fi
 else   
 download_latest_version
 fi
-
-echo $text > $extract_dir/misc/announce.txt
