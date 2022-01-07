943658e0f49a90ad23dabf239d6ce6e08ecacf1d
pedroz <sdustpedro@gmail.com>
2022-01-08 00:27:33 +0800

    fix

--- scripts/misc/mtcrack.sh
+++ scripts/misc/mtcrack.sh
@@ -22,28 +22,27 @@ description=${string_antimoduledescription}
 version=1.0
 versionCode=1" >> $TEMP_DIR/module.prop
 
-echo '#!/system/bin/sh
+echo "#!/system/bin/sh
 chmod 731 /data/system/theme
-rm -rf /data/system/package_cache/*' >>$TMPDIR/service.sh
+rm -rf /data/system/package_cache/*" >> $TEMP_DIR/service.sh
 
-echo '#!/system/bin/sh
+echo "#!/system/bin/sh
 rm -rf /data/system/package_cache/*
 chmod 775 /data/system/theme
-rm -rf /data/system/theme/rights' >>$TMPDIR/uninstall.sh
+rm -rf /data/system/theme/rights" >> $TEMP_DIR/uninstall.sh
 
   mkdir -p $MODPATH
-  cp -rf $FAKEMODPATH/. $MODPATH
   set_perm_recursive $MODPATH 0 0 0755 0644
-  cp -af $TMPDIR/module.prop /data/adb/modules_update/antimithemerestore/module.prop
-  cp -af $TMPDIR/service.sh /data/adb/modules_update/antimithemerestore/service.sh
-  cp -af $TMPDIR/uninstall.sh /data/adb/modules_update/antimithemerestore/uninstall.sh
+  cp -af $TEMP_DIR/module.prop /data/adb/modules_update/antimithemerestore/module.prop
+  cp -af $TEMP_DIR/service.sh /data/adb/modules_update/antimithemerestore/service.sh
+  cp -af $TEMP_DIR/uninstall.sh /data/adb/modules_update/antimithemerestore/uninstall.sh
+  cp -af $TEMP_DIR/module.prop /data/adb/modules/antimithemerestore/module.prop
   mktouch /data/adb/modules/antimithemerestore/update
   rm -rf \
   $MODPATH/system/placeholder $MODPATH/customize.sh \
   $MODPATH/README.md $MODPATH/.git* 2>/dev/null
   cd /
   rm -rf $TEMP_DIR/*
-  echo ""
   echo "${string_installsuccess}"
   echo "---------------------------------------------"
   exit 0
