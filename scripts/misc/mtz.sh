18df4d244e877db46f921ae8809d11f83791e0e5
pzcn <sdustpedro@gmail.com>
2022-08-31 17:56:15 +0800

    Update mtz.sh

--- scripts/misc/mtz.sh
+++ scripts/misc/mtz.sh
@@ -101,7 +101,7 @@ addon(){
   source $TEMP_DIR/link.ini
   http_code="`curl -I -s --connect-timeout 1 ${link_check} -w %{http_code} | tail -n1`" 
 if [ "$http_code" != null ];then
-    if [ ! "$httpcode" == *$http_code* ]; then
+    if [ ! "$httpcode" =~ $http_code ]; then
     {  echo "${string_nonetworkdetected}" && rm -rf $TEMP_DIR/* >/dev/null && exit 1; }
   fi
 else
