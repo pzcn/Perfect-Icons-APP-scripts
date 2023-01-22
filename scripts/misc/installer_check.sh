172a50276cd173952bd13613ac439497a6862ca2
pzcn <sdustpedro@gmail.com>
2023-01-22 21:45:55 +0800

    Update installer_check.sh

--- scripts/misc/installer_check.sh
+++ scripts/misc/installer_check.sh
@@ -18,7 +18,7 @@ elif [ "$1" == kernelsu ]; then
 		echo 0
 	fi
 elif [ "$1" == magisk ]; then 
-	if [ `magisk -V` -lt 20400 ]; then
+	if [ `magisk -V` -ge 20400 ]; then
 		system_check
 	else
 		echo 0
