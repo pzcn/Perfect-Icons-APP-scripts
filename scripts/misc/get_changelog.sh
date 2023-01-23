a872ee7f4d3b7a4d156d9eee131a63e6c9627d51
pzcn <sdustpedro@gmail.com>
2023-01-23 13:39:21 +0800

    Update get_changelog.sh

--- scripts/misc/get_changelog.sh
+++ scripts/misc/get_changelog.sh
@@ -1,3 +1,4 @@
 #!/system/bin/sh
 curl -skLJo "$TEMP_DIR/changelog.xml" https://miuiicons-generic.pkg.coding.net/icons/files/changelog.xml?version=latest
+cp "$START_DIR/online-scripts/pic/push.png" "$TEMP_DIR/push.png"
 echo "$TEMP_DIR/changelog.xml"
