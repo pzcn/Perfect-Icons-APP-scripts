d1e089de73f00497035711dfef3cdade14204d61
pzcn <sdustpedro@gmail.com>
2023-10-25 00:39:25 +0800

    Update get_changelog.sh

--- scripts/misc/get_changelog.sh
+++ scripts/misc/get_changelog.sh
@@ -2,4 +2,19 @@
 curl -skLJo "$TEMP_DIR/changelog.xml" https://miuiicons-generic.pkg.coding.net/icons/files/changelog2.xml?version=latest
 cp -rf "$START_DIR/online-scripts/pic/push.png" "$TEMP_DIR/push.png"
 cp -rf "$START_DIR/online-scripts/pic/update.png" "$TEMP_DIR/update.png"
-echo "$TEMP_DIR/changelog.xml"
+cat >$TEMP_DIR/1.xml <<'EOF'
+<?xml version="1.0" encoding="UTF-8" ?>
+<group>
+<page html="https://iconsx.pedroz.eu.org/push.html"  icon="push.png">
+<title>订阅新版本通知</title>
+<desc>通过微信订阅后，有新版本会推送通知</desc>
+</page>
+<action icon="update.png">
+<title>检查更新</title>
+<set>source $START_DIR/online-scripts/misc/update_check.sh</set>
+<desc sh="source online-scripts/misc/get_count.sh"/>
+</action>
+</group>
+EOF
+cat "$TEMP_DIR/changelog.xml" >> "$TEMP_DIR/1.xml"
+echo "$TEMP_DIR/1.xml"
