13d9c96984130bc04c1fec135c3aa506681aa7a7
pzcn <sdustpedro@gmail.com>
2023-10-25 20:59:56 +0800

    Update am.sh

--- scripts/misc/am.sh
+++ scripts/misc/am.sh
@@ -15,13 +15,15 @@ if [ "$1" = "--version" ]; then
 fi
 
 # If sdk version is not exported by app, then get value with getprop instead.
-if ! is_int "$ANDROID__BUILD_VERSION_SDK"; then
-    ANDROID__BUILD_VERSION_SDK="$(getprop "ro.build.version.sdk")"
-    if ! is_int "$ANDROID__BUILD_VERSION_SDK"; then
+if ! is_int "$ANDROID_SDK"; then
+    ANDROID_SDK="$(getprop "ro.build.version.sdk")"
+    if ! is_int "$ANDROID_SDK"; then
         echo "Failed to get android build version sdk with getprop" 1>&2
         exit 1
     fi
 fi
+
+user="$(id -u)"
 
 # Do not use TermuxAm for broadcast commands on Android >= 14 since it
 # will trigger the `Sending broadcast <action> with resultTo requires resultToApp`
@@ -29,8 +31,8 @@ fi
 # app_process command forever since no result callback is received
 # by TermuxAm `IIntentReceiver.performReceive()`.
 # - https://github.com/termux/TermuxAm/issues/9#issuecomment-1649867810
-if [ "$ANDROID__BUILD_VERSION_SDK" -ge 34 ] && [ "$1" = "broadcast" ]; then
-    is_int "$TERMUX__USER_ID" || TERMUX__USER_ID=0
+if [ "$ANDROID_SDK" -ge 34 ] && [ "$1" = "broadcast" ]; then
+    is_int "$user" || user=0
     shift 1 # Remove the first `broadcast` argument
 
     # Use a subshell with specified redirection to prevent
@@ -40,7 +42,7 @@ if [ "$ANDROID__BUILD_VERSION_SDK" -ge 34 ] && [ "$1" = "broadcast" ]; then
     # `pty` devices when a source transition is made from `untrusted_app*`
     # domain when `/system/bin/am` is executed.
     # - https://github.com/termux/termux-packages/discussions/8292#discussioncomment-5102555
-    output="$(/system/bin/am broadcast --user "$TERMUX__USER_ID" "$@" 2>&1 </dev/null)"
+    output="$(/system/bin/am broadcast --user "$user" "$@" 2>&1 </dev/null)"
     exit_code=$?
     # Do not log `echo: I/0 error` if stdout is closed
     echo "$output" 2>/dev/null
@@ -59,7 +61,6 @@ fi
 # - https://cs.android.com/android/_/android/platform/art/+/d3a8a9e960d533f39b6bafc785599eae838a6351
 # - https://cs.android.com/android/_/android/platform/art/+/03ac3eb0fc36be97f301ac60e85e1bb7ca52fa12:runtime/native/dalvik_system_DexFile.cc;l=335
 if [ -w "$AM_APK_PATH" ]; then
-    user="$(id -u)"
     if [ "$user" != "0" ] && [ "$user" != "1000" ] && [ "$user" != "2000" ]; then
         chmod 0400 "$AM_APK_PATH" || exit $?
     fi
