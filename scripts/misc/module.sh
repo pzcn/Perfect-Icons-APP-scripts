1f5d98cb7de9f97423762b3ec1ac15d47eeb0ddf
pzcn <sdustpedro@gmail.com>
2023-10-28 05:58:30 +0800

    Update module.sh

--- scripts/misc/module.sh
+++ scripts/misc/module.sh
@@ -131,28 +131,28 @@ if [ $var_miui_version -lt 10 ]; then
 fi
 
 if [ -L "/system/media" ] ;then
-  MEDIAPATH=/system$(realpath /system/media)
+  MEDIAPATH=system$(realpath /system/media)
 else
   if [ -d "/system/media" ]; then 
-    MEDIAPATH=/system/media
+    MEDIAPATH=system/media
   else
     abort "- ROM似乎有问题，无法安装。"
     abort "- There seems to be a problem with the ROM and it cannot be installed."
   fi
 fi
 
-REPLACE="$MEDIAPATH/theme/miui_mod_icons"
+REPLACE="/$MEDIAPATH/theme/miui_mod_icons"
 
 echo "- 安装中..."
 echo "- installing..."
 
-mkdir -p ${MODPATH}${MEDIAPATH}/theme/default/
+mkdir -p ${MODPATH}/${MEDIAPATH}/theme/default/
 unzip -oj "$ZIPFILE" icons -d $MODPATH/$MEDIAPATH/theme/default/ >&2
 if [ $var_miui_version -gt 14 ]; then 
-  mkdir -p "$MEDIAPATH/theme/miui_mod_icons"
+  mkdir -p "${MODPATH}/${MEDIAPATH}/theme/miui_mod_icons"
   for i in icon_background icon_border icon_folder icon_folder_light icon_mask icon_pattern; do
-    unzip "$MODPATH/$MEDIAPATH/theme/default/icons" "res/drawable-xxhdpi/$i.png" -d "$MEDIAPATH/theme/miui_mod_icons"
-    zip -d "$MEDIAPATH/theme/miui_mod_icons" "res/drawable-xxhdpi/$i.png"
+    unzip "$MODPATH/$MEDIAPATH/theme/default/icons" "res/drawable-xxhdpi/$i.png" -d "$MODPATH/$MEDIAPATH/theme/miui_mod_icons"
+    zip -d "$MODPATH/$MEDIAPATH/theme/default/icons" "res/drawable-xxhdpi/$i.png"
   done
 fi
 unzip -oj "$ZIPFILE" addons/* -d $MODPATH/$MEDIAPATH/theme/default/ >&2
